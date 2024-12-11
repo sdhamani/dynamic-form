4c311f39c153e77204005dc58cb9e373a31dd0ca
Sagar Dhamani <sagar.dhamani@acko.tech>
2024-12-12 01:27:17 +0530

    Added field id to dependency

--- src/components/common/inputfield/index.tsx
+++ src/components/common/inputfield/index.tsx
@@ -37,7 +37,7 @@ const InputField: React.FC<InputFieldProps> = React.memo(({
             error: !!error,
             helperText: error || "",
         }),
-        [field.label, value, onChange, error]
+        [field.id, field.label, value, onChange, error]
     );
 
     const renderInput = React.useCallback(() => {
