8391d784a375cafbe1675a256b367659a474e23b
Sagar Dhamani <sagar.dhamani@acko.tech>
2024-12-11 20:05:49 +0530

    Added validateField utils

---
+++ src/utils/validationInput.ts
@@ -0,0 +1,51 @@
+import { FieldConfig } from "../components/common/inputfield";
+
+export const validateField = (
+    field: FieldConfig,
+    value: string | boolean,
+    isVisible: (field: FieldConfig) => boolean,
+    dispatch: React.Dispatch<any>
+): boolean => {
+    if (!isVisible(field)) {
+        dispatch({
+            type: "SET_ERROR",
+            payload: {
+                fieldId: field.id,
+                error: null,
+            },
+        });
+        return true;
+    }
+
+    if (field.required && (value === undefined || value === false || value === "")) {
+        dispatch({
+            type: "SET_ERROR",
+            payload: {
+                fieldId: field.id,
+                error: field.errorMessage || `${field.label} is required.`,
+            },
+        });
+        return false;
+    }
+
+    if (field.regex && typeof value === "string" && !new RegExp(field.regex).test(value)) {
+        dispatch({
+            type: "SET_ERROR",
+            payload: {
+                fieldId: field.id,
+                error: field.errorMessage || "Invalid value.",
+            },
+        });
+        return false;
+    }
+
+    dispatch({
+        type: "SET_ERROR",
+        payload: {
+            fieldId: field.id,
+            error: null,
+        },
+    });
+
+    return true;
+};
